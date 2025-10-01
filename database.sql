cdf7b6d8391f1e060655a4a7a83ec6025065536f
soatmurotov1 <soatmurotovabrorbek23@gmail.com>
2025-10-01 14:08:47 +0500

    almost done

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE users_db;
+CREATE DATABASE users;
 
 CREATE TABLE users_db(
-    id SERIAL PRIMARY KEY,
+    id serial PRIMARY KEY,
     first_name VARCHAR(200),
-    email VARCHAR(200),
+    email VARCHAR(200) UNIQUE,
     last_name VARCHAR(200),
-    password VARCHAR(200),
+    password VARCHAR(200) NOT NULL,
     phone_number varchar(200),
     address varchar(200)
 );
@@ -20,18 +20,17 @@ CREATE TABLE posts_db(
     title VARCHAR(200),
     content TEXT,
     slug VARCHAR(200),
-    user_id VARCHAR(200)
+    user_id VARCHAR(200) REFERENCES users_db(id) 
 );
 
 
 
 
-CREATE TABLE commnt(
+CREATE TABLE comments_db(
     id SERIAL PRIMARY KEY,
     content TEXT,
-    post_id VARCHAR(200),
-    user_id VARCHAR(200),
-
+    post_id VARCHAR(200) REFERENCES posts_db(id),
+    user_id VARCHAR(200) REFERENCES users_db(id)
 );
 
 
